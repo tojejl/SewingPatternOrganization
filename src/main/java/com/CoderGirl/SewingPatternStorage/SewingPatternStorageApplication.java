df1f0f92fe237b1328c9a5b4a40b3137ef9dbac1
jennifer toje <Jennifer>
2019-11-01 15:18:38 -0500

    First Commit, Initialized directory from start.spring.io

---
+++ src/main/java/com/CoderGirl/SewingPatternStorage/SewingPatternStorageApplication.java
@@ -0,0 +1,13 @@
+package com.CoderGirl.SewingPatternStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewingPatternStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewingPatternStorageApplication.class, args);
+	}
+
+}
